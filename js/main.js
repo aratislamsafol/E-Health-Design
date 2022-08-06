d8ea89c4110cf0c1e33d04f75b7063312590cd2c
aratislamsafol <aratislamsafol@gmail.com>
2022-08-07 03:39:42 +0600

    Top nav, Navbar complete, Banner running

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let menuToggle=document.querySelector('.toggle_btn');
+menuToggle.onclick=function(){
+    menuToggle.classList.toggle('active');
+}
+
+let btn_close_toggle=document.querySelector('.btn-close');
+    btn_close_toggle.onclick=function(){
+    menuToggle.classList.toggle('active');
+}
