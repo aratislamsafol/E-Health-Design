3e4feed1b8fe5d123d7b322a4dd68660cbd07fa6
aratislamsafol <aratislamsafol@gmail.com>
2022-08-09 02:51:02 +0600

    Achivement Complete

--- js/main.js
+++ js/main.js
@@ -7,3 +7,46 @@ let btn_close_toggle=document.querySelector('.btn-close');
     btn_close_toggle.onclick=function(){
     menuToggle.classList.toggle('active');
 }
+
+// Filter
+$(document).ready(function(){
+
+  $(".filter-button").click(function(){
+      var value = $(this).attr('data-filter');
+      
+      if(value == "all")
+      {
+          //$('.filter').removeClass('hidden');
+          $('.filter').show('1000');
+      }
+      else
+      {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+          $(".filter").not('.'+value).hide('3000');
+          $('.filter').filter('.'+value).show('3000');
+          
+      }
+  });
+  
+  if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+// Counter
+$(document).ready(function(){
+  $('.counter-value').each(function(){
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      },{
+          duration: 3500,
+          easing: 'swing',
+          step: function (now){
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
+});
